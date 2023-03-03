052864bab69d62e2d200263c3186ed16434450e1
Lintang Digdoyo <digdoyo.lintang@gmail.com>
2023-03-03 19:19:35 +0700

    try commit with unused import

--- apps/market-web/components/Common/Typography/__tests__/Typography.test.tsx
+++ apps/market-web/components/Common/Typography/__tests__/Typography.test.tsx
@@ -1,4 +1,4 @@
-import { screen, render } from "@testing-library/react";
+import { screen, render, AllByRole } from "@testing-library/react";
 import "@testing-library/jest-dom";
 
 import { TypographyType, TypographyVariantType } from "../types";
