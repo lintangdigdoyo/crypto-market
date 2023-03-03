e43d07c224e3a9592a9f382603219b67526b70c1
Lintang Digdoyo <digdoyo.lintang@gmail.com>
2023-03-03 13:45:31 +0700

    fix: dependencies conflict between apps

--- apps/market-web/next.config.js
+++ apps/market-web/next.config.js
@@ -1,7 +1,5 @@
-//@ts-check
-
 // eslint-disable-next-line @typescript-eslint/no-var-requires
-const { withNx } = require("@nrwl/next/plugins/with-nx");
+const withNx = require("@nrwl/next/plugins/with-nx");
 
 /**
  * @type {import('@nrwl/next/plugins/with-nx').WithNxOptions}
